0f84e20a153967a3c6f4cd66f52bbb582e44af07
Krystian Otto <otto.krystiann@gmail.com>
2020-10-15 17:21:18 +0200

    add basic app methods(get, post, delete etc)

--- server.js
+++ server.js
@@ -1,7 +1,47 @@
 const express = require('express');
+const { v1: uuidv1 } = require('uuid');
+
+const db = [
+  { id: 1, author: 'John Doe', text: 'This company is worth every coin!' },
+  { id: 2, author: 'Amanda Doe', text: 'They really know how to make you happy.' },
+];
 
 const app = express();
 
-app.listen(8000, () => {
-  console.log('Server running');
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Home Page');
 });
+
+app.get('/testimonials', (req, res) => {
+  res.send(db);
+});
+
+app.get('/testimonials/:id', (req, res) => {
+  res.send(db.filter( elem => elem.id == req.params.id));
+});
+
+app.get('/testimonials/random', () => {
+  res.send(db.filter( elem => elem.id == uuidv1()));
+});
+
+app.post('/testimonials', ( req, res) => {
+  req.body.id = uuidv1();
+  db.push(req.body);
+});
+
+app.put('/testimonials/:id', (req ,res) => {
+  db.find( x => x.id == req.params.id).author = req.body.author;
+  db.find( x => x.id == req.params.id).text = req.body.text;
+});
+
+app.delete('/testimonials/:id', (req, res) => {
+  const deletedElem = db.find( x => x.id == req.params.id);
+  const indexOfDeletedElem = db.indexOf(deletedElem);
+  db.splice(indexOfDeletedElem, 1);
+});
+
+
+app.listen(8000);
